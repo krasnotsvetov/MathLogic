5b48cf075a160eedb1dc947d4f5181cf2d5bf0ea
Vladimir <vvkraf@gmail.com>
2017-03-11 15:41:30 +0300

    Add E solution

--- ProblemE/Program.cs
+++ ProblemE/Program.cs
@@ -11,10 +11,75 @@ namespace ProblemE
     {
         static void Main(string[] args)
         {
-            using (var sr = new StreamReader(new FileStream("base.proof", FileMode.Open))
-            {
+            int a, b;
 
-            };
+            using (var sr = new StreamReader(new FileStream("test.in", FileMode.Open)))
+            {
+                var s = sr.ReadLine().Split(' ').Select(t => int.Parse(t)).ToList();
+                a = s[0];
+                b = s[1];
+            }
+
+
+            List<string> proofs = new List<string>();
+            using (var sr = new StreamReader(new FileStream("base.proof", FileMode.Open)))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null) {
+                    proofs.Add(line);
+                }
+            }
+
+            string numeralFromInt(int num)
+            {
+                var sb = new StringBuilder();
+                sb.Append("0");
+                for (int i = 0; i < num; i++)
+                {
+                    sb.Append("'");
+                }
+                return sb.ToString();
+            }
+            string numeralA = numeralFromInt(a);
+            using (var sw = new StreamWriter(new FileStream("test.out", FileMode.Create)))
+            {
+                sw.WriteLine($"|-{numeralFromInt(a)}+{numeralFromInt(b)}={numeralFromInt(a + b)}");
+                using (var sr = new StreamReader(new FileStream("base2.proof", FileMode.Open)))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+                for (int i = 0; i < b; i++)
+                {
+                    string numeralB = numeralFromInt(i);
+                    string numeralC = numeralFromInt(i + a);
+                    foreach (var l in proofs)
+                    {
+                        foreach (var c in l)
+                        {
+                            switch (c)
+                            {
+                                case 'd':
+                                    sw.Write(numeralA);
+                                    break;
+                                case 'e':
+                                    sw.Write(numeralB);
+                                    break;
+                                case 'f':
+                                    sw.Write(numeralC);
+                                    break;
+                                default:
+                                    sw.Write(c);
+                                    break;
+                            }
+                        }
+                        sw.WriteLine();
+                    }
+                }
+            }
 
               
         }
